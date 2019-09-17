4e9fba2978d09c5ea90fb96f54fd53fa1e371f97
Emily_Richard <richard.emily.e@gmail.com>
2019-09-17 15:46:02 -0400

    Finished part 1

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom';
 import axios from 'axios';
 
 const MovieList = props => {
@@ -21,6 +22,7 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
+        <Link 
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
